9371f5276fb6c03cb78927af01dab979c6e1fb32
Eduard Leandro Correa Avendaño <3du4rd@gmail.com>
2021-08-18 15:55:07 -0500

    feat: :sparkles: 498. Configuring TypeScript

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+"use strict";
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
-    console.log(result);
-});
+if (buttonElement) {
+    buttonElement.addEventListener('click', function () {
+        var num1 = num1Element.value;
+        var num2 = num2Element.value;
+        var result = add(+num1, +num2);
+        console.log(result);
+    });
+}
