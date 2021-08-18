c7915d843304ba2568c2a7e08d86f7c684746d22
Eduard Leandro Correa Avendaño <3du4rd@gmail.com>
2021-08-18 16:12:27 -0500

    feat: :sparkles: 502. Understanding Generics

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 "use strict";
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
-var numResults = [];
-var textResults = [];
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const buttonElement = document.querySelector('button');
+const numResults = [];
+const textResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -16,13 +16,21 @@ function add(num1, num2) {
 function printResult(resultObj) {
     console.log(resultObj.val);
 }
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
+buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
     numResults.push(result);
-    var stringResult = add(num1, num2);
+    const stringResult = add(num1, num2);
     textResults.push(stringResult);
     printResult({ val: result, timestamp: new Date() });
     console.log(numResults, textResults);
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It worked!');
+    }, 5000);
+});
+myPromise.then((result) => {
+    console.log(result.split('w'));
 });
--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-const numResults: number[]  = [];
+const numResults: Array<number> = [];
 const textResults: string[] = [];
 
 type NumOrString = number | string;
@@ -36,4 +36,14 @@ buttonElement.addEventListener('click', () => {
   textResults.push(stringResult as string);
   printResult({ val: result as number, timestamp: new Date() });
   console.log(numResults, textResults);
+});
+
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It worked!');
+  }, 5000);
+});
+
+myPromise.then((result) => {
+  console.log(result.split('w'));
 });
