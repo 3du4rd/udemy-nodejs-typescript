28f35553f8a1104b69499bdc6db0ccf1e63e6ab3
Eduard Leandro Correa Avendaño <3du4rd@gmail.com>
2021-08-18 15:58:43 -0500

    feat: :sparkles: 499. Working with Union Types

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-if (buttonElement) {
-    buttonElement.addEventListener('click', function () {
-        var num1 = num1Element.value;
-        var num2 = num2Element.value;
-        var result = add(+num1, +num2);
-        console.log(result);
-    });
-}
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    var stringResult = add(num1, num2);
+    console.log(result);
+    console.log(stringResult);
+});
