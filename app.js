4656ac24bca16ff508e18723475d1462642877f7
Eduard Leandro Correa Avendaño <3du4rd@gmail.com>
2021-08-18 16:03:08 -0500

    feat: :sparkles: 500. Using Object & Array Types

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
+var numResults = [];
+var textResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -11,11 +13,16 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResults.push(result);
     var stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
+const numResults: number[]  = [];
+const textResults: string[] = [];
+
 function add(num1: number | string, num2: number | string) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
@@ -11,11 +14,18 @@ function add(num1: number | string, num2: number | string) {
   return +num1 + +num2;
 }
 
+function printResult(resultObj: { val: number; timestamp: Date }) {
+  console.log(resultObj.val);
+}
+
+
 buttonElement.addEventListener('click', () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
+  numResults.push(result as number);
   const stringResult = add(num1, num2);
-  console.log(result);
-  console.log(stringResult);
+  textResults.push(stringResult as string);
+  printResult({ val: result as number, timestamp: new Date() });
+  console.log(numResults, textResults);
 });
